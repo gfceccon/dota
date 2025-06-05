21b6a4102617210c5f0dcb768a0082ac9692d29f
Gustavo Ceccon <gustavofceccon@gmail.com>
2025-06-05 01:04:59 -0300

    Alterações em organização e hero embedding wip

--- dataset.py
+++ dataset.py
@@ -1,25 +1,58 @@
+import ast
 import kagglehub
 import polars as pl
+from heroes import get_heroes
 from patches import get_patches
-from preprocessing import preprocess_dota_dataset
+from matches import get_matches
+from players import get_players_draft, game_cols
+from players import game_cols
+import ast
 
 
+def preprocess_dataset(path: str, patches: list[int], tier: list[str],
+                       min_duration: int = 10 * 60, max_duration: int = 120 * 60) -> pl.LazyFrame:
+    # Carregando e filtrando partidas
+    matches = get_matches(path, patches, tier, min_duration, max_duration)
 
+    # Carregando jogos e draft de jogadores
+    games = get_players_draft(path, matches)
 
-def get_dataset(path: str, patches: list[int], tier: list[str] = ["professional"], 
-                min_duration: int = 10 * 60, max_duration: int = 120*60) -> pl.DataFrame:
-    
-    dataset = preprocess_dota_dataset(
-        path,
-        patches,
-        tier,
-        min_duration,
-        max_duration
-    )
-    
+    # Agrupando dados por partida
+    dataset = (
+        games.group_by("match_id")
+        .agg(
+            pl.all().drop_nulls(),
+            
+            pl.when(pl.col("team") == 0, pl.col("pick")).then(
+                pl.col("hero")).drop_nulls().alias("radiant_picks"),
+            pl.when(pl.col("team") == 1, pl.col("pick")).then(
+                pl.col("hero")).drop_nulls().alias("dire_picks"),
+            
+            pl.when(pl.col("team") == 0, ~pl.col("pick")).then(
+                pl.col("hero")).drop_nulls().alias("radiant_bans"),
+            pl.when(pl.col("team") == 1, ~pl.col("pick")).then(
+                pl.col("hero")).drop_nulls().alias("dire_bans"),
+            
+            pl.concat_list([pl.col(f"{col}").max()
+                           for col in game_cols]).alias("max_stats"),
+        ))
+
     return dataset
 
-def save_dataset(dataset: pl.DataFrame, output_path: str):
+
+def get_dataset(path: str, years: tuple[int, int] = (2023, 2024), tier: list[str] = ['professional'], duration: tuple[int, int] = (30, 120)) -> pl.DataFrame:
+    patches = get_patches(path, begin_year=years[0], end_year=years[1])
+    dataset = preprocess_dataset(
+        path,
+        list(patches.keys()),
+        tier,
+        duration[0] * 60,
+        duration[1] * 60
+    )
+    return dataset.collect()
+
+
+def save_dataset(dataset: pl.DataFrame, output_path: str = "./tmp/DATASET.json") -> None:
     print(f"Salvando dataset em {output_path}...")
     dataset.write_json(output_path)
     print("Dataset salvo com sucesso!")
@@ -33,9 +66,9 @@ if __name__ == "__main__":
     print(f"Dataset baixado para: {path}")
 
     # Definir patches e tier
-    patches = get_patches(path, begin_year=2023, end_year=2024) # Patches disponíveis
-    tier = ["professional"] # Tier de interesse
-    min_duration=10 * 60  # 10 minutos em segundos
+    patches = get_patches(path, begin_year=2023,
+                          end_year=2024)  # Patches disponíveis
+    tier = ["professional"]  # Tier de interesse
 
     print(f"Patches disponíveis: {patches}")
     sum = 0
@@ -43,10 +76,10 @@ if __name__ == "__main__":
         sum += count
     print(f"- Total de jogos: {sum}")
 
-    
     # Carregar e pré-processar o dataset
-    dataset = get_dataset(path, list(patches.keys()), tier, min_duration)
-    
+    dataset = get_dataset(path, (2023, 2024),
+                          tier, duration=(30, 120))
+
     # Mostrar informações do dataset
     print(f"\nDataset pré-processado:")
     print(f"- Número de matches: {len(dataset)}")
@@ -56,7 +89,7 @@ if __name__ == "__main__":
     sample = dataset.sample()
     print(f"\nExibindo dados do match_id {sample["match_id"][0]}:")
     print(sample)
-    
+
     # Salvar dataset processado
     output_path = "./tmp/processed_dataset.json"
     save_dataset(dataset.head(5), output_path)
--- players.py
+++ players.py
@@ -4,24 +4,31 @@ from files import (
     players_file,
     picks_bans_file,
 )
+from heroes import get_heroes
+
+players_cols = [
+    "kills", "deaths", "assists",
+    "hero_id", "player_slot", "account_id",
+    "obs_placed", "sen_placed",
+    "gold_per_min", "xp_per_min",
+    "hero_damage", "tower_damage", "hero_healing",
+    "roshan_kills", "tower_kills",
+]
+
+picks_cols = [
+    "is_pick",
+    "team",
+    "hero_id",
+    "order",
+]
+
+game_cols = ["player_gold_per_min", "player_xp_per_min",
+             "player_kills", "player_deaths", "player_assists"]
+hero_cols = ["hero_id", "hero_name", "hero_primary_attr",
+             "hero_attack_type", "hero_roles"]
 
 
 def get_players_draft(path: str, matches: pl.LazyFrame) -> pl.LazyFrame:
-
-    players_cols = [
-        "kills", "deaths", "assists",
-        "hero_id", "player_slot", "account_id",
-        "obs_placed", "sen_placed",
-        "gold_per_min", "xp_per_min",
-        "hero_damage", "tower_damage", "hero_healing",
-        "roshan_kills", "tower_kills",
-    ]
-
-    picks_cols = [
-        "is_pick",
-        "team",
-        "hero_id",
-    ]
 
     players = (
         pl.scan_csv(f"{path}/{players_file}")
@@ -34,41 +41,62 @@ def get_players_draft(path: str, matches: pl.LazyFrame) -> pl.LazyFrame:
         .select([pl.col(col).alias(f"pick_{col}") for col in picks_cols] + ["match_id"])
     )
 
+    heroes = get_heroes(path).select(
+        pl.col("hero_id").alias("pick_hero_id"),
+        pl.col("hero_name").alias("pick_hero_name"),
+        pl.col("primary_attr").alias("pick_primary_attr"),
+        pl.col("roles").alias("pick_roles")
+    ).lazy()
+
     games = (
         matches
         .join(picks, on="match_id", how="inner")
         .join(players, left_on=["match_id", "pick_hero_id"], right_on=["match_id", "player_hero_id"], how="left")
+        .join(heroes, left_on="pick_hero_id", right_on="hero_id", how="inner")
         .with_columns([
-            # Hero assignments based on pick/ban and team
-            pl.when(~pl.col("pick_is_pick"))
-              .then(pl.col("pick_hero_id"))
-              .alias("ban_hero_id"),
-            
-            pl.when(pl.col("pick_is_pick") & (pl.col("pick_team") == 0))
-              .then(pl.col("pick_hero_id"))
-              .alias("radiant_hero_id"),
-            
-            pl.when(pl.col("pick_is_pick") & (pl.col("pick_team") == 1))
-              .then(pl.col("pick_hero_id"))
-              .alias("dire_hero_id"),
-            
+            # Renomeia as colunas para facilitar o acesso
+            pl.col("pick_team").alias("team"),
+            pl.col("pick_hero_id").alias("hero"),
+            pl.col("pick_is_pick").alias("pick"),
+            pl.col("pick_order").alias("order"),
+            pl.col("hero_id"),
+
+
             # Team-based stats assignment using dictionary comprehension
             *[
-                pl.when(pl.col("pick_team") == team_id)
-                  .then(pl.col(f"player_{stat}"))
-                  .alias(f"{team_name}_{stat}")
+                pl.when(pl.col("pick_team") == team_id,
+                        pl.col("pick_is_pick") == True)
+                .then(
+                    pl.concat_list(
+                        [f"{stat}" for stat in game_cols]
+                    ).alias(f"player_stats"))
+                .otherwise(pl.lit(None))
+                .alias(f"{team_name}_stats")
                 for team_id, team_name in [(0, "radiant"), (1, "dire")]
-                for stat in ["gold_per_min", "xp_per_min", "kills", "deaths", "assists"]
-            ]
+            ],
+            *[
+                pl.when(pl.col("pick_team") == team_id,
+                        pl.col("pick_is_pick") == True)
+                .then(
+                    pl.concat_list(
+                        [f"{stat}" for stat in hero_cols]
+                    ).alias(f"hero_stats"))
+                .otherwise(pl.lit(None))
+                .alias(f"{team_name}_hero_stats")
+                for team_id, team_name in [(0, "radiant"), (1, "dire")]
+            ],
         ])
         .select([
             "match_id",
-            "radiant_hero_id", "dire_hero_id", "ban_hero_id",
-            "radiant_kills", "dire_kills",
-            "radiant_deaths", "dire_deaths", 
-            "radiant_assists", "dire_assists",
-            "radiant_gold_per_min", "dire_gold_per_min",
-            "radiant_xp_per_min", "dire_xp_per_min",
+            "hero_id", "hero_name"
+            "team", "order",
+            "hero", "pick",
+            "radiant_stats",
+            "radiant_hero_stats",
+            "dire_stats",
+            "dire_hero_stats",
+            "pick_order",
+            *game_cols
         ])
     )
 
