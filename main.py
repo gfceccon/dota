6aaa00923781c942a9c07d7f3fb05e51d6e85493
Gustavo Ceccon <gustavofceccon@gmail.com>
2025-06-06 19:52:02 -0300

    Autoencoder finalizado, otimização e testes wip

--- main.py
+++ main.py
@@ -15,31 +15,40 @@ def main():
     dataset_name = "bwandowando/dota-2-pro-league-matches-2023"
     path = kagglehub.dataset_download(dataset_name)
     dataset, player_cols, hero_cols = get_dataset(path)
+    
+    heroes, _, dict_roles = get_heroes(path)
+    n_heroes = heroes.select("hero_id").max().collect().item()
+    for roles in dict_roles:
+        dict_roles[roles] = dict_roles[roles]
+    n_roles = max(dict_roles.values())
+    n_hero_stats = len(hero_cols)
+    n_player_stats = len(player_cols)
+    print (f"Total Hero Stats: {n_hero_stats}, Total Player Stats: {n_player_stats}")
+    print(f"Total Roles: {n_roles}, Total Heroes: {n_heroes}")
+    print(f"Total Heroes: {n_heroes}, Total Roles: {len(dict_roles)}")
+
     if("--save" in sys.argv):
         save_dataset(dataset.head())
         return
     
-    heroes, _, dict_roles = get_heroes(path)
-    n_heroes = heroes.select("hero_id").max().collect().item()
-    print(f"Total Heroes: {n_heroes}, Total Roles: {len(dict_roles)}")
     autoencoder = Dota2Autoencoder(
-        player_stats=player_cols,
-        hero_roles=hero_cols,
-        dict_roles=dict_roles,
         hero_pick_embedding_dim=16,
         hero_role_embedding_dim=8,
-        n_heroes=n_heroes,
+        n_player_stats=n_player_stats,
+        n_heroes=n_heroes + 1,
+        n_roles=n_roles + 1,
         n_players=5,
         n_bans=7,
         latent_dim=32,
         hidden_layers=[128, 64],
         dropout=0.2,
-        learning_rate=0.001
+        learning_rate=0.001,
+        verbose=True
     )
     
     print("Training Dota2 Autoencoder...")
     
-    autoencoder.train_data(dataset, 10)
+    autoencoder.train_data(dataset, 10, verbose=False)
     
     print("Training completed.")
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -8,11 +8,10 @@ import polars as pl
 class Dota2Autoencoder(nn.Module):
     def __init__(
         self,
-        player_stats: list[str],
-        hero_roles: list[str],
-        dict_roles: dict[str, int],
         hero_pick_embedding_dim: int,
         hero_role_embedding_dim: int,
+        n_player_stats: int,
+        n_roles: int,
         n_heroes: int,
         n_players: int = 10,
         n_bans: int = 14,
@@ -20,31 +19,26 @@ class Dota2Autoencoder(nn.Module):
         hidden_layers: list[int] = [128, 64],
         dropout: float = 0.2,
         learning_rate: float = 0.001,
+        verbose: bool = False
     ):
         super(Dota2Autoencoder, self).__init__()
         # Configura o device para GPU se disponível, caso contrário CPU
         self.device = torch.device(
             "cuda" if torch.cuda.is_available() else "cpu")
 
-        # Store the provided data
-        self.game_stats = player_stats
-        self.hero_stats = hero_roles
-        self.dict_roles = dict_roles
-
         # Número de heróis, estatísticas, jogadores e bans
         self.n_heroes = n_heroes
-        self.n_stats = len(hero_roles)  # Calculate from players_stats
+        self.n_player_stats = n_player_stats
         self.n_players = n_players
         self.n_bans = n_bans
         self.n_picks = n_players + n_bans
-        # Calculate from dict_attributes
-        self.n_roles = len(dict_roles)  # Calculate from dict_roles
+        self.n_roles = n_roles
 
         # Camadas de embedding para heróis, bans e estatísticas
         self.hero_pick_embedding = nn.Embedding(
-            n_heroes + 1, hero_pick_embedding_dim, device=self.device)
+            n_heroes, hero_pick_embedding_dim, device=self.device)
         self.hero_role_embedding = nn.Embedding(
-            self.n_roles, hero_pick_embedding_dim, device=self.device)
+            self.n_roles, hero_role_embedding_dim, device=self.device)
 
         # Dimensões e hiperparâmetros do modelo
         self.latent_dim = latent_dim
@@ -58,34 +52,38 @@ class Dota2Autoencoder(nn.Module):
         # Calcula a dimensão de entrada do modelo, para cada time
         self.input_dim = self.compute_input_dim()
 
-        self.encoder = self.create_encoder()
-        self.decoder = self.create_encoder(decoder=True)
+        self.encoder = self.create_encoder(verbose=verbose)
+        self.decoder = self.create_encoder(decoder=True, verbose=verbose)
         self.optimizer = torch.optim.Adam(self.parameters(), learning_rate)
         self.loss = nn.MSELoss()
         self.loss_history = []
 
     def compute_input_dim(self):
-        picks_dim = self.n_players * self.hero_embedding_dim
-        # Para cada ban: hero_embedding
-        bans_dim = self.n_bans * self.hero_embedding_dim
-        # Para cada jogador: stats
-        stats_dim = self.n_players * self.n_stats
-        hero_role_dim = self.n_players * self.hero_role_embedding_dim
-        return picks_dim + bans_dim + stats_dim
+        picks_dim = 2 * self.n_players * self.hero_embedding_dim
+        bans_dim = 2 * self.n_bans * self.hero_embedding_dim
+        stats_dim = 2 * self.n_players * self.n_player_stats
+        hero_role_dim = 2 * self.n_players * self.n_roles * self.hero_role_embedding_dim
+        self.input_dim = picks_dim + bans_dim + stats_dim + hero_role_dim
+        return self.input_dim
 
-    def create_encoder(self, decoder: bool = False):
+    def create_encoder(self, decoder: bool = False, verbose: bool = False) -> nn.Sequential:
         layers = []
         hidden_layers = self.hidden_layers if not decoder else reversed(
             self.hidden_layers)
         dimension = self.input_dim if not decoder else self.latent_dim
         for _hidden in hidden_layers:
+            if verbose:
+                print(
+                    f"Adding layer with input dimension: {dimension}, output dimension: {_hidden}")
             layers.extend([
                 nn.Linear(dimension, _hidden, device=self.device),
                 nn.ReLU(),
                 nn.Dropout(self.dropout),
-                nn.BatchNorm1d(_hidden, device=self.device)
             ])
             dimension = _hidden
+        if verbose:
+            print(
+                f"Adding final layer with input dimension: {dimension}, output dimension: {self.latent_dim if not decoder else self.input_dim}")
         layers.append(
             nn.Linear(dimension, self.latent_dim if not decoder else self.input_dim, device=self.device))
         return nn.Sequential(*layers).to(self.device)
@@ -94,6 +92,7 @@ class Dota2Autoencoder(nn.Module):
                 radiant_bans: list[int], dire_bans: list[int],
                 radiant_hero_roles: list[list[int]], dire_hero_roles: list[list[int]],
                 radiant_stats: list[list[float]], dire_stats: list[list[float]],
+                verbose: bool = False
                 ) -> torch.Tensor:
 
         # Picks e bans
@@ -106,30 +105,61 @@ class Dota2Autoencoder(nn.Module):
         dire_bans_feat: torch.Tensor = self.hero_pick_embedding(
             torch.tensor(dire_bans, device=self.device))
 
-        radiant_hero_roles_feat = [
-            self.hero_role_embedding(torch.tensor(
-                roles, dtype=torch.long, device=self.device))
-            for roles in radiant_hero_roles]
-        dire_hero_roles_feat = [
-            self.hero_role_embedding(torch.tensor(
-                roles, dtype=torch.long, device=self.device))
-            for roles in dire_hero_roles]
+        def pad_roles(roles):
+            return roles + [0] * (self.n_roles - len(roles))
 
-        radiant_stats_feat = [torch.tensor(stats, device=self.device) for stats in radiant_stats]
-        dire_stats_feat = [torch.tensor(stats, device=self.device) for stats in dire_stats]
+        radiant_hero_roles_padded = [pad_roles(roles)
+                                     for roles in radiant_hero_roles]
+        dire_hero_roles_padded = [pad_roles(roles)
+                                  for roles in dire_hero_roles]
+
+        radiant_hero_roles_tensor = torch.tensor(
+            radiant_hero_roles_padded, dtype=torch.long, device=self.device)
+        dire_hero_roles_tensor = torch.tensor(
+            dire_hero_roles_padded, dtype=torch.long, device=self.device)
+
+        radiant_hero_roles_feat: torch.Tensor = self.hero_role_embedding(
+            radiant_hero_roles_tensor)
+        dire_hero_roles_feat: torch.Tensor = self.hero_role_embedding(
+            dire_hero_roles_tensor)
+
+        # Stats são normalizados e convertidos em tensores
+        radiant_stats_feat: list[torch.Tensor] = [torch.tensor(
+            stats, device=self.device) for stats in radiant_stats]
+        dire_stats_feat: list[torch.Tensor] = [torch.tensor(
+            stats, device=self.device) for stats in dire_stats]
+
+        if (verbose):
+            print(
+                f"Radiant picks shape: {radiant_picks_feat.shape}, Dire picks shape: {dire_picks_feat.shape}")
+            print(
+                f"Radiant bans shape: {radiant_bans_feat.shape}, Dire bans shape: {dire_bans_feat.shape}")
+
+            for stats in radiant_hero_roles_feat:
+                print(f"Radiant Hero roles tensor shape: {stats.shape}")
+            for stats in radiant_stats_feat:
+                print(f"Radiant Stats tensor shape: {stats.shape}")
+            for stats in dire_hero_roles_feat:
+                print(f"Dire Hero roles tensor shape: {stats.shape}")
+            for stats in dire_stats_feat:
+                print(f"Dire Stats tensor shape: {stats.shape}")
 
         # Flatten nested lists of tensors for stats and hero roles
-        radiant_stats_feat = torch.cat(radiant_stats_feat, dim=0).flatten()
-        dire_stats_feat = torch.cat(dire_stats_feat, dim=0).flatten()
-        radiant_hero_roles_feat = torch.cat(radiant_hero_roles_feat, dim=0).flatten()
-        dire_hero_roles_feat = torch.cat(dire_hero_roles_feat, dim=0).flatten()
+        flat = torch.cat([
+            radiant_picks_feat.view(-1),
+            dire_picks_feat.view(-1),
+            radiant_bans_feat.view(-1),
+            dire_bans_feat.view(-1),
+            radiant_hero_roles_feat.view(-1),
+            dire_hero_roles_feat.view(-1),
+            torch.cat(radiant_stats_feat).view(-1),
+            torch.cat(dire_stats_feat).view(-1)
+        ]).unsqueeze(0)
 
-        return torch.cat([
-            radiant_picks_feat.flatten(), dire_picks_feat.flatten(),
-            radiant_bans_feat.flatten(), dire_bans_feat.flatten(),
-            radiant_stats_feat, dire_stats_feat,
-            radiant_hero_roles_feat, dire_hero_roles_feat
-        ], dim=0).unsqueeze(0)
+        if (verbose):
+            print(f"Flattened tensor shape: {flat.shape}")
+
+        return flat
 
     def forward(self, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
         # Encoda e decodifica os dados
@@ -144,14 +174,16 @@ class Dota2Autoencoder(nn.Module):
     def train_step(self, radiant_picks: list[int], dire_picks: list[int],
                    radiant_bans: list[int], dire_bans: list[int],
                    radiant_hero_roles: list[list[int]], dire_hero_roles: list[list[int]],
-                   radiant_stats: list[list[float]], dire_stats: list[list[float]]
+                   radiant_stats: list[list[float]], dire_stats: list[list[float]],
+                   verbose: bool = False
                    ) -> float:
         self.train()
         self.optimizer.zero_grad()
         original = self.flatten(radiant_picks, dire_picks,
                                 radiant_bans, dire_bans,
                                 radiant_hero_roles, dire_hero_roles,
-                                radiant_stats, dire_stats)
+                                radiant_stats, dire_stats,
+                                verbose)
         latent, reconstructed = self.forward(original)
         loss = self.loss(original, reconstructed)
         loss.backward()
@@ -159,7 +191,7 @@ class Dota2Autoencoder(nn.Module):
         self.loss_history.append(loss.item())
         return loss.item()
 
-    def train_data(self, training_df: pl.DataFrame, epochs: int = 10) -> None:
+    def train_data(self, training_df: pl.DataFrame, epochs: int = 10, verbose=False) -> None:
         for epoch in range(epochs):
             total_loss = 0.0
             for row in training_df.iter_rows(named=True):
@@ -173,21 +205,29 @@ class Dota2Autoencoder(nn.Module):
                 dire_hero_roles = row['dire_hero_roles']
 
                 radiant_stats: list[list[float]] = row['radiant_stats']
-                dire_stats: list[list[float]]  = row['dire_stats']
+                dire_stats: list[list[float]] = row['dire_stats']
 
                 min_stats = np.array(row['min_stats'])
                 max_stats = np.array(row['max_stats'])
-                
+
                 diff_stats = max_stats - min_stats
-                diff_stats = np.where(diff_stats == 0, 1, diff_stats)  # Evita divisão por zero
-                
-                radiant_stats = [((np.array(stats) - min_stats) / diff_stats).tolist() for stats in radiant_stats]
-                dire_stats = [((np.array(stats) - min_stats) / diff_stats).tolist() for stats in dire_stats]
+                # Evita divisão por zero
+                diff_stats = np.where(diff_stats == 0, 1, diff_stats)
+
+                radiant_stats = [
+                    ((np.array(stats) - min_stats) / diff_stats).tolist() for stats in radiant_stats]
+                dire_stats = [((np.array(stats) - min_stats) /
+                               diff_stats).tolist() for stats in dire_stats]
 
                 loss = self.train_step(
                     radiant_picks, dire_picks, radiant_bans, dire_bans,
                     radiant_hero_roles, dire_hero_roles,
-                    radiant_stats, dire_stats)
+                    radiant_stats, dire_stats,
+                    verbose
+                )
                 total_loss += loss
             avg_loss = total_loss / len(training_df)
-            print(f'Epoch {epoch + 1}/{epochs}, Loss: {avg_loss:.4f}')
+            if (verbose):
+                print(f'Epoch {epoch + 1}/{epochs}, Loss: {avg_loss:.4f}')
+            elif (epoch % 10 == 0):
+                print(f'Epoch {epoch + 1}/{epochs}, Loss: {avg_loss:.4f}')
