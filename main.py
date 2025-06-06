c310e4ba41f2e407b51fad41715f7cd8f47c0ed3
Gustavo Ceccon <gustavofceccon@gmail.com>
2025-06-06 02:02:31 -0300

    Model changes

--- main.py
+++ main.py
@@ -1,34 +1,46 @@
 import kagglehub
 import polars as pl
 import numpy as np
+import json
+import torch
 from dataset import get_dataset, save_dataset
+from heroes import get_heroes
 from model import Dota2Autoencoder
 from typing import Dict, List, Tuple
+import sys
 
 
 def main():
-    # Download do dataset
+    # Carregar metadados dos heróis
     dataset_name = "bwandowando/dota-2-pro-league-matches-2023"
     path = kagglehub.dataset_download(dataset_name)
-
-
-    dataset = get_dataset(path)
-
-    # autoencoder = Dota2Autoencoder(
-    #     n_heroes=n_heroes,
-    #     hero_embedding_dim=16,
-    #     n_stats=len(game_cols),
-    #     team_embedding_dim=8,
-    #     n_players=10,
-    #     n_picks=14,
-    #     latent_dim=32,
-    #     hidden_layers=[128, 64],
-    #     dropout=0.2,
-    #     learning_rate=0.001
-    # )
-
-    print(dataset)
-
-
+    dataset, player_cols, hero_cols = get_dataset(path)
+    if("--save" in sys.argv):
+        save_dataset(dataset.head())
+        return
+    
+    heroes, _, dict_roles = get_heroes(path)
+    n_heroes = heroes.select("hero_id").max().collect().item()
+    print(f"Total Heroes: {n_heroes}, Total Roles: {len(dict_roles)}")
+    autoencoder = Dota2Autoencoder(
+        player_stats=player_cols,
+        hero_roles=hero_cols,
+        dict_roles=dict_roles,
+        hero_pick_embedding_dim=16,
+        hero_role_embedding_dim=8,
+        n_heroes=n_heroes,
+        n_players=5,
+        n_bans=7,
+        latent_dim=32,
+        hidden_layers=[128, 64],
+        dropout=0.2,
+        learning_rate=0.001
+    )
+    
+    print("Training Dota2 Autoencoder...")
+    
+    autoencoder.train_data(dataset, 10)
+    
+    print("Training completed.")
 if __name__ == "__main__":
     main()
--- players.py
+++ players.py
@@ -6,33 +6,33 @@ from files import (
 )
 from heroes import get_heroes
 
-players_cols = [
-    "kills", "deaths", "assists",
-    "hero_id", "player_slot", "account_id",
-    "obs_placed", "sen_placed",
-    "gold_per_min", "xp_per_min",
-    "hero_damage", "tower_damage", "hero_healing",
-    "roshan_kills", "tower_kills",
-]
 
-picks_cols = [
-    "is_pick",
-    "team",
-    "hero_id",
-    "order",
-]
+def get_players_draft(path: str, matches: pl.LazyFrame) -> tuple[pl.LazyFrame, list[str], list[str]]:
 
-game_cols = ["player_gold_per_min", "player_xp_per_min",
-             "player_kills", "player_deaths", "player_assists"]
-hero_cols = ["hero_id", "hero_name", "hero_primary_attr",
-             "hero_attack_type", "hero_roles"]
+    players_cols = [
+        "kills", "deaths", "assists",
+        "hero_id", "player_slot", "account_id",
+        "obs_placed", "sen_placed",
+        "gold_per_min", "xp_per_min",
+        "hero_damage", "tower_damage", "hero_healing",
+        "roshan_kills", "tower_kills",
+    ]
 
+    picks_cols = [
+        "is_pick",
+        "team",
+        "hero_id",
+        "order",
+    ]
 
-def get_players_draft(path: str, matches: pl.LazyFrame) -> pl.LazyFrame:
+    game_cols = ["player_gold_per_min", "player_xp_per_min",
+                 "player_kills", "player_deaths", "player_assists"]
+    hero_cols = ["primary_attribute", "attack_type", "roles", ]
 
     players = (
         pl.scan_csv(f"{path}/{players_file}")
         .select([pl.col(col).alias(f"player_{col}") for col in players_cols] + ["match_id"])
+        .with_columns(pl.col("player_hero_id").alias("hero_id"))
     )
 
     picks = (
@@ -40,26 +40,18 @@ def get_players_draft(path: str, matches: pl.LazyFrame) -> pl.LazyFrame:
         .drop_nulls(subset="team")
         .select([pl.col(col).alias(f"pick_{col}") for col in picks_cols] + ["match_id"])
     )
-
-    heroes = get_heroes(path).select(
-        pl.col("hero_id").alias("pick_hero_id"),
-        pl.col("hero_name").alias("pick_hero_name"),
-        pl.col("primary_attr").alias("pick_primary_attr"),
-        pl.col("roles").alias("pick_roles")
-    ).lazy()
-
+    heroes, _, _ = get_heroes(path)
     games = (
         matches
         .join(picks, on="match_id", how="inner")
-        .join(players, left_on=["match_id", "pick_hero_id"], right_on=["match_id", "player_hero_id"], how="left")
-        .join(heroes, left_on="pick_hero_id", right_on="hero_id", how="inner")
+        .with_columns(pl.col("pick_hero_id").alias("hero_id"))
+        .join(players, left_on=["match_id", "hero_id"], right_on=["match_id", "hero_id"], how="left")
+        .join(heroes, on="hero_id", how="inner")
         .with_columns([
             # Renomeia as colunas para facilitar o acesso
             pl.col("pick_team").alias("team"),
-            pl.col("pick_hero_id").alias("hero"),
-            pl.col("pick_is_pick").alias("pick"),
+            pl.col("pick_is_pick").cast(pl.Int32).alias("pick"),
             pl.col("pick_order").alias("order"),
-            pl.col("hero_id"),
 
 
             # Team-based stats assignment using dictionary comprehension
@@ -68,7 +60,7 @@ def get_players_draft(path: str, matches: pl.LazyFrame) -> pl.LazyFrame:
                         pl.col("pick_is_pick") == True)
                 .then(
                     pl.concat_list(
-                        [f"{stat}" for stat in game_cols]
+                        [pl.col(f"{stat}") * 1.0 for stat in game_cols]
                     ).alias(f"player_stats"))
                 .otherwise(pl.lit(None))
                 .alias(f"{team_name}_stats")
@@ -82,22 +74,22 @@ def get_players_draft(path: str, matches: pl.LazyFrame) -> pl.LazyFrame:
                         [f"{stat}" for stat in hero_cols]
                     ).alias(f"hero_stats"))
                 .otherwise(pl.lit(None))
-                .alias(f"{team_name}_hero_stats")
+                .alias(f"{team_name}_hero_roles")
                 for team_id, team_name in [(0, "radiant"), (1, "dire")]
             ],
-        ])
-        .select([
+        ]).select([
             "match_id",
-            "hero_id", "hero_name"
-            "team", "order",
-            "hero", "pick",
+            "hero_id",
+            "hero_name",
+            "team",
+            "order",
+            "pick",
             "radiant_stats",
-            "radiant_hero_stats",
+            "radiant_hero_roles",
             "dire_stats",
-            "dire_hero_stats",
-            "pick_order",
+            "dire_hero_roles",
             *game_cols
         ])
     )
 
-    return games
+    return games, game_cols, hero_cols
